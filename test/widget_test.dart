24d57da544bbf7c3946f09f442a3a2252672d724
Martin Paredes <quantumvalley@gmail.com>
2020-03-03 12:52:15 -0600

    Fix archivo test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:latos/main.dart';
+import 'package:latos/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
