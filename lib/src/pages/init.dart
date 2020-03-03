ae26a616fc90d60ceaee0fbbfd01478d7828d634
Martin Paredes <quantumvalley@gmail.com>
2020-03-03 12:51:46 -0600

    Formación estructural y plugin carousel-pro (primera aproximación)

---
+++ lib/src/pages/init.dart
@@ -0,0 +1,98 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Carousel',
+      home: ImageCarousel(),
+    );
+  }
+}
+
+class ImageCarousel extends StatefulWidget {
+  _ImageCarouselState createState() => new _ImageCarouselState();
+}
+
+class _ImageCarouselState extends State<ImageCarousel> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = new Tween(begin: 0.0, end: 18.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          // the state that has changed here is the animation object’s value
+        });
+      });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    Widget carousel = new Carousel(
+      boxFit: BoxFit.cover,
+      images: [
+        NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+        NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+        ExactAssetImage('assets/images/img_01.jpg'),
+      ],
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(seconds: 1),
+    );
+
+    Widget banner = new Padding(
+      padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+        child: new Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0)),
+            color: Colors.amber.withOpacity(0.5),
+          ),
+          padding: const EdgeInsets.all(10.0),
+          child: new Text(
+            'Banner on top of carousel',
+            style: TextStyle(
+              fontFamily: 'fira',
+              fontSize: animation.value,//18.0,
+              //color: Colors.white,
+            ),
+          ),
+        ),
+      // ),
+    //  ),
+    );
+
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Center(
+        child: new Container(
+          padding: EdgeInsets.only(top: 150.0, bottom: 100.0),
+          height: screenHeight,
+          child: new ClipRRect(
+            borderRadius: BorderRadius.circular(0.0),
+            child: new Stack(
+              children: [
+                carousel,
+                banner,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
