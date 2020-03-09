d960a5fd74a859e460f51136dad916a32163f9f6
Martin Paredes <quantumvalley@gmail.com>
2020-03-09 11:15:34 -0600

    Status Bar transparente

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:latos/src/pages/init.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.light
+  ));
+  runApp(MyApp());
+}
 
